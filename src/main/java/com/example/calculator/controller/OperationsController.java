a3afbed7e9cf082a6a4ad8b95a581119c3f0fb4f
Daria Gromova <igromova81@gmail.com>
2022-09-12 22:55:00 +0300

    Дз

--- src/main/java/com/example/calculator/controller/OperationsController.java
+++ src/main/java/com/example/calculator/controller/OperationsController.java
@@ -2,6 +2,7 @@ package com.example.calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,19 +20,20 @@ public class OperationsController {
         return "Добро пожаловать в калькулятор";
     }
     @GetMapping("/plus")
-    public CharSequence plus(int num1, int num2) {
+    public CharSequence plus(@RequestParam int num1, @RequestParam int num2) {
         return num1 + "+" + num2 + "=" + operationsService.getPlus(num1, num2);
     }
     @GetMapping("/minus")
-    public CharSequence minus(int num1, int num2) {
+    public CharSequence minus(@RequestParam int num1, @RequestParam int num2) {
         return num1 + "-" + num2 + "=" + operationsService.getMinus(num1, num2);
     }
     @GetMapping("/multiply")
-    public CharSequence multiply(int num1, int num2) {
+    public CharSequence multiply(@RequestParam int num1, @RequestParam int num2) {
         return num1 + "*" + num2 + "=" + operationsService.getMultiply(num1, num2);
     }
     @GetMapping("/divide")
-    public CharSequence divide(int num1, int num2) {
+    public CharSequence divide(@RequestParam int num1, @RequestParam int num2) throws RuntimeException {
+        if (num2 == 0) return "Ошибка. Деление на ноль";
         return num1 + "/" + num2 + "=" + operationsService.getDivide(num1, num2);
     }
 }
